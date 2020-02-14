4168d45b7930b3a6d237c6bd9c618620815071bf
sh-soltanpour <shahryar2006@gmail.com>
2020-02-15 00:05:43 +0330

    Prevent providing duplicate restaurants and foods

--- src/main/kotlin/com/ie/loghme/controllers/RestaurantController.kt
+++ src/main/kotlin/com/ie/loghme/controllers/RestaurantController.kt
@@ -17,7 +17,11 @@ class RestaurantController(
 
     @GetMapping
     fun getRestaurants(): List<Restaurant> {
-        return restaurantRepository.findAll().shuffled().take(30)
+        return restaurantRepository.findAll()
+                .distinctBy { it.name }
+                .filter { it.menu.isNotEmpty() }
+                .shuffled()
+                .take(30)
     }
 
     @PostMapping("/reload")
@@ -29,8 +33,23 @@ class RestaurantController(
         if (request.getHeader("X-Auth-Token") != secretToken)
             response.status = 403
         else {
+            restaurants.forEach { it.menu = it.menu.distinctBy { food -> food.name } }
             restaurantRepository.saveAll(restaurants)
         }
+    }
+
+    @GetMapping("/duplicates")
+    fun getDuplicateFoods(
+            request: HttpServletRequest,
+            response: HttpServletResponse
+    ): List<Map<String, Int>> {
+        if (request.getHeader("X-Auth-Token") != secretToken) {
+            response.status = 403
+            return emptyList()
+        }
+        return restaurantRepository.findAll()
+                .map { it.menu.groupBy { food -> food.name } }
+                .map { map -> map.mapValues { it.value.size } }
     }
 
     @DeleteMapping
